1880a94516f2e6a3515f638db49d1fa4ff7630fc
VR SREEGANESH <35948310+kunjaps@users.noreply.github.com>
2020-05-19 01:54:26 +0530

    Update Run_analysis.R

--- Run_analysis.R
+++ Run_analysis.R
@@ -21,18 +21,18 @@ TidyData$code <- activities[TidyData$code,2]
 
 # naming things in the right way
 names(TidyData)[2] = "activity"
-names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData) <-gsub("Acc", "Accelerometer", names(TidyData))
 names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
 names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData) <-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData) <- gsub("^t", "Time", names(TidyData))
 names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<- gsub("tBody", "TimeBody", names(TidyData))
 names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<- gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
 names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+names(TidyData)   <-gsub("gravity", "Gravity", names(TidyData))
 
 # independant tidy dataset with average of each variable
 FinalData <- TidyData %>% group_by(subject, activity) %>% summarise_all(funs(mean))
